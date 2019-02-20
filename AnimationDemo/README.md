3e37623bfe6d13199a3982e028f3180158de14da
KuoPingL <kuopingl+1@gmail.com>
2019-02-20 13:33:28 +0800

    Added Animation Demo

---
+++ AnimationDemo/README.md
@@ -0,0 +1,7 @@
+# AnimationDemo
+Just an educational demos for CoreAnimation
+Here are the DEMOs available :
+1. CATransform3D (30%)
+
+Future DEMOs :
+1. ...
---
+++ InstagramProfilePage/InstagramProfilePage/DianChanTapCopy/DianChanTapCopyViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DianChanTapCopyViewController.swift
+//  InstagramProfilePage
+//
+//  Created by Jimmy on 2019/2/15.
+//  Copyright © 2019 Jimmy. All rights reserved.
+//
+
+import Foundation
