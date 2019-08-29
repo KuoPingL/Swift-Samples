a00457a567c24b58f68de0b9c8301a50145e45b6
KuoPingL <kuopingl+1@gmail.com>
2019-08-29 16:25:54 +0800

    Pass data to tableview

--- JimmyProblems/Chatroom/DialogInputViewDemo/DialogInputViewDemo/Scenes/ChatRoom/SimpleChatRoomLogDelegate.swift
+++ JimmyProblems/Chatroom/DialogInputViewDemo/DialogInputViewDemo/Scenes/ChatRoom/SimpleChatRoomLogDelegate.swift
@@ -10,5 +10,44 @@ import Foundation
 import UIKit
 
 class SimpleChatRoomLogDelegate: NSObject, UITableViewDelegate, UITableViewDataSource {
-//    var 
+    
+    private var messages:[SimpleChatModel] = []
+    private weak var tableView: UITableView?
+    private weak var delegate: UIViewController?
+    
+    init(tableView: UITableView, delegate: UIViewController) {
+        super.init()
+        self.tableView = tableView
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(SimpleChatCell.self, forCellReuseIdentifier: SimpleChatCell.cellID)
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+        
+        self.delegate = delegate
+    }
+    
+    public func appendMessage(_ message: SimpleChatModel) {
+        messages.append(message)
+        tableView?.reloadData()
+        tableView?.layoutIfNeeded()
+        tableView?.scrollToRow(at: IndexPath(row: messages.count - 1, section: 0), at: .bottom, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: SimpleChatCell.cellID) as? SimpleChatCell
+        if cell == nil {
+            cell = SimpleChatCell(style: .default, reuseIdentifier: SimpleChatCell.cellID)
+        }
+        
+        cell?.chat = messages[indexPath.row]
+        
+        return cell!
+    }
+    
 }
