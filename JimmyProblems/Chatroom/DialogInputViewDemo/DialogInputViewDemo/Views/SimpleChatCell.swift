2b598082641a779112641aa0af539bba6a2faeda
KuoPingL <kuopingl+1@gmail.com>
2019-08-29 16:23:52 +0800

    Fixed: label constraints

--- JimmyProblems/Chatroom/DialogInputViewDemo/DialogInputViewDemo/Views/SimpleChatCell.swift
+++ JimmyProblems/Chatroom/DialogInputViewDemo/DialogInputViewDemo/Views/SimpleChatCell.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class SimpleChatCell: UITableViewCell {
     static let cellID = "CHAT_CELL"
-    var chat: SimpeChatModel? {
+    var chat: SimpleChatModel? {
         didSet {
             guard let chat = chat else {
                 return
@@ -21,8 +21,69 @@ class SimpleChatCell: UITableViewCell {
                 
             } else {
                 textLabel?.text = chat.message
+                textLabel?.numberOfLines = 0
+                isSender = chat.isSender
             }
         }
+    }
+    
+    var isSender: Bool = false {
+        didSet {
+            
+            textLabelConstraint?.isActive = false
+            if isSender {
+                textLabelConstraint = textLabel?.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -20)
+                
+                DispatchQueue.main.async {
+                    self.textLabel?.textColor = .black
+                    self.textLabel?.backgroundColor = UIColor.orange
+                }
+                
+            } else {
+                textLabelConstraint = textLabel?.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20)
+                DispatchQueue.main.async {
+                    self.textLabel?.textColor = .white
+                    self.textLabel?.backgroundColor = UIColor.black
+                }
+            }
+            textLabelConstraint?.isActive = true
+        }
+    }
+    
+    private lazy var customizedLabel: UILabel = {
+        return InsetUILabel()
+    }()
+    
+    override var textLabel: UILabel? {
+        get {
+            return customizedLabel
+        }
+    }
+    
+    private var textLabelConstraint: NSLayoutConstraint?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(textLabel!)
+        textLabel?.translatesAutoresizingMaskIntoConstraints = false
+        textLabel?.layer.cornerRadius = 15.0
+        textLabel?.layer.masksToBounds = true
+        textLabel?
+            .top.equalTo(contentView.top_safeAreaLayoutGuid,
+                         constant: 10)
+            .bottom.equalTo(contentView.bottom_safeAreaLayoutGuide,
+                            constant: -10)
+            .width.lessThanOrEqualTo(nil,
+                                     constant: contentView.bounds.width * 3.0 / 4.0)
     }
     
 }
