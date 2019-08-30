f53c52f1d8b1767d08982e056c1d44780cc45db2
KuoPingL <kuopingl+1@gmail.com>
2019-08-30 10:10:45 +0800

    Add Images to Extension

--- JimmyProblems/Chatroom/DialogInputViewDemo/DialogInputViewDemo/Extensions/UIImageExtension.swift
+++ JimmyProblems/Chatroom/DialogInputViewDemo/DialogInputViewDemo/Extensions/UIImageExtension.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIImage {
+    static var avatar_A: UIImage? {
+        get {
+           return UIImage(named: "Avatar_A", in: nil, compatibleWith: nil)
+        }
+    }
+    
+    static var avatar_B: UIImage? {
+        get {
+            return UIImage(named: "Avatar_B", in: nil, compatibleWith: nil)
+        }
+    }
+    
+    static var bubbleReceived: UIImage? {
+        get {
+            return UIImage(named: "Bubble_Received", in: nil, compatibleWith: nil)
+        }
+    }
+    
+    static var bubbleSend: UIImage? {
+        get {
+            return UIImage(named: "Bubble_Sent", in: nil, compatibleWith: nil)
+        }
+    }
+    
+    static var send: UIImage? {
+        get {
+            return UIImage(named: "Send", in: nil, compatibleWith: nil)
+        }
+    }
+    
+    static var microphone: UIImage? {
+        get {
+            return UIImage(named: "Microphone", in: nil, compatibleWith: nil)
+        }
+    }
+}
